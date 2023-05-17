ce23fa04767032015599e5e3c6b12bffedc3c2f6
Karishma Damania <starkarish@icloud.com>
2023-05-17 11:27:42 +0400

    Terraform Basics Added

---
+++ 01-terraform-basics/main.tf
@@ -0,0 +1,18 @@
+
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-karishmadamania-001"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
